543b6728fdc3d10592a31ee80d037f56fd5c4a17
zack altschuler <altschulerzack@yahoo.com>
2020-09-21 01:26:12 -0700

    Update Header.js

--- src/TODO/Header.js
+++ src/TODO/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="headerDiv">
       <header>
-        <h2 className="header">Todo list !</h2>
+        <h2 className="header">Todo list</h2>
       </header>
     </div>
   );
